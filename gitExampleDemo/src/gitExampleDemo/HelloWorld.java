44302e9890185f81d74c36ca85071874a90e5ae2
Laxman Chamlagai <lc21994@vccsstudents.onmicrosoft.com>
2021-08-09 15:35:46 -0400

    Added constructor in the helloWorld class

--- gitExampleDemo/src/gitExampleDemo/HelloWorld.java
+++ gitExampleDemo/src/gitExampleDemo/HelloWorld.java
@@ -4,6 +4,16 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		
+	}
+
+	private String firstName;
+	private String lastName;
+	
+	public HelloWorld(String firstName, String lastName) {
+		this.firstName = firstName;
+		this.lastName =lastName;
+		
 	}
 
 }
